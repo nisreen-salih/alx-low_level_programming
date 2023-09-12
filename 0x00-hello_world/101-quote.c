0f07651ca0cec015b5ee573e53c97832a49e970b
Nisreen <fg.nisreen@gmail.com>
2023-09-12 16:39:47 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * description : printing sayntex
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
