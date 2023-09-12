084c312d948777890a0c641a1fb58305027e2dac
Nisreen <fg.nisreen@gmail.com>
2023-09-11 19:22:18 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: alphabet
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	for (int a = 'a'; a < 'z'; a++)
+	{
+		putchar (a);
+	}
+	return (0);
+}
