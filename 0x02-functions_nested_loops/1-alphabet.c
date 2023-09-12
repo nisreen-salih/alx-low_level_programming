8b4961d7187d7087db0beaf7a992bd21dae42f99
Nisreen <fg.nisreen@gmail.com>
2023-09-12 16:17:40 -0700

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main – entry point
+*
+* description : alpha
+*
+* Return: always 0
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
