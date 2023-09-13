a791eed11258c7329561580985b00883172be248
Nisreen <fg.nisreen@gmail.com>
2023-09-12 17:30:49 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description : reverse alpha
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char z = 'z';
+
+	while ( z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
