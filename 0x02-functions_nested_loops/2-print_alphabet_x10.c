9358a52e13859af45bcb638ab01286d6669a0b04
Nisreen <fg.nisreen@gmail.com>
2023-09-15 20:11:59 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int l, c;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (c = 'a'; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
