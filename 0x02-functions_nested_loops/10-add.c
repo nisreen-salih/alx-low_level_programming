ab9e09438846473cd3be11c944d6a4412a9c3c8e
Nisreen <fg.nisreen@gmail.com>
2023-09-17 10:03:30 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - add two numbs
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
+
