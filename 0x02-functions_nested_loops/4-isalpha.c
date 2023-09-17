a21c02078432d785680c20f0fc00452a397b3f29
Nisreen <fg.nisreen@gmail.com>
2023-09-17 09:14:16 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * _isalpha - checks for alphabets
+ *
+ * @c : input
+ *
+ *Return: 1 if c i letter 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if (c >= 65 && c <= 90)
+	{
+		return (o1);
+	}
+	else
+	{
+		return (0);
+	}
+}
