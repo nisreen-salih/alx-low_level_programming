8ec33544ac28a06a472ce491fc3d24e501a2064f
Nisreen <fg.nisreen@gmail.com>
2023-09-17 11:01:08 -0700

    14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : prints the first 50 Fibonacci numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c;
+	unsigned long f1 = 0, f2 = 1, s;
+
+	for (c = 0 ; c < 50; c++)
+	{
+		s = f1 + f2;
+		printf("%lu", s);
+
+		f1 = f2;
+		f2 = s;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
