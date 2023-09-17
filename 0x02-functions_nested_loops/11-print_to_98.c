f821d624a69a23ba3684a6459e80758c459ecfb9
Nisreen <fg.nisreen@gmail.com>
2023-09-17 10:13:14 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints num for n to 98
+ *
+ * @n: input
+ *
+ */
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n > 98)
+	{
+		for (a = n; a > 98; a--)
+		{
+			printf("%d. ", a);
+		}
+	}
+	else
+	{
+		for (a = n; a < 98; a++)
+		{
+			printf("%d. ", a);
+		}
+	}
+	printf("98\n");
+}
