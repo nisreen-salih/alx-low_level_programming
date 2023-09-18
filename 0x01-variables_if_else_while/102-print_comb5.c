2d3a24bb40d73599981a8a61a1f76c4c75d2adf5
Nisreen <fg.nisreen@gmail.com>
2023-09-17 18:25:41 -0700

    12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descrition : num comb
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int d1 = 0, d2;
+
+	while (d1 <= 99)
+	{
+		d2 = d1;
+		while (d2 <= 99)
+		{
+			if (d2 != d1)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+
+				if (d1 != 98 || d2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
