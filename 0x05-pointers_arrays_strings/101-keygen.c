2bc4e5035c059f6b30b0f4cd058c8992a369a757
Nisreen <fg.nisreen@gmail.com>
2023-09-18 16:00:51 -0700

    11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,5 +10,16 @@
 
 int main(void)
 {
-	
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
