d1bff5efaf4b644f624cab7f1c843a282333c17b
Nisreen <fg.nisreen@gmail.com>
2023-09-21 16:27:00 -0700

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @s: input
+ *
+ * Return: encoded
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	char ret13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstubwxyzabcdefghijklNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == ret13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
+}
