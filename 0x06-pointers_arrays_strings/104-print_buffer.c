a1419310903aeb68516ef48a428a429f9a7ea7b9
Nisreen <fg.nisreen@gmail.com>
2023-09-21 17:32:59 -0700

    12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * pASCII - determine ascii char
+ *
+ * @n: int
+ *
+ * Return: result
+ */
+
+int pASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * pHEXA - print hex
+ * @b: input
+ * @s : inout
+ * @e : end
+ */
+
+void pHEXA(char *b, int s, int e)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < e)
+		{
+			printf("%02x", *(b + s + i));
+		}
+		else
+		{
+			printf(" ");
+		}
+		if (i % 2)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ *
+ * @b: input
+ * @size: the size
+ *
+ * Return: 0
+ */
+
+void print_buffer(char *b, int size)
+{
+	int start, end;
+
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			pHEXA(b, start, end);
+			pASCII(b, start, end);
+			printf("\n");
+		}
+	} else
+		printf("\n");
+}
