d63ecd4fb4b13b03aff7898b8d7f07be72f3a1d4
Nisreen <fg.nisreen@gmail.com>
2023-09-25 06:04:09 -0700

    7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,13 @@
 
 void print_diagsums(int *a, int size)
 {
-	int l = 0, c1 = 0, c2 = 0;
+	int l, c1 = 0, c2 = 0;
 
-	while (l < size)
+	for (l = 0; l < size; l++)
 	{
-		c1 += a[l];
-		c2 += a[size - l - 1];
-		a += size;
-		l++;
+		c1 = c1 + a[l];
+		c2 = c2 + a[size - l - 1];
+		a = a + size;
 	}
 	printf("%d, ", c1);
 	printf("%d\n, ", c2);
