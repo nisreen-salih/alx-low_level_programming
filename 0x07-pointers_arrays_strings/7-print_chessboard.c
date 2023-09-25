9ac2f0c5ec5fa74f2c3af06acfc70be7a70b2504
Nisreen <fg.nisreen@gmail.com>
2023-09-25 05:35:57 -0700

    6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,11 +10,11 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int b = 0, w = 0;
+	int b, w;
 
-	for (b < 8; b++;)
+	for (b = 0; b < 8; b++)
 	{
-		for (w < 8; w++;)
+		for (w = 0; w < 8; w++)
 		{
 			_putchar(a[b][w]);
 		}
