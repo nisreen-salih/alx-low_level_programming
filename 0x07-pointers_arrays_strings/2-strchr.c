3230c066441d9615a6329b0d0deceddb09e1f8b2
Nisreen <fg.nisreen@gmail.com>
2023-09-25 04:48:00 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string
+ *
+ * @s: string
+ * @c: char
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int ch;
+
+	do {
+		if (s[ch] == c)
+			return (s + ch);
+	ch++;
+	}
+	while (s[ch] >= '\0');
+	return ('\0');
+}
