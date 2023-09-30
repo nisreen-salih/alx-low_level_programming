737c7d651a28a98f837db300385a08c22eed8386
Nisreen <fg.nisreen@gmail.com>
2023-09-30 05:34:55 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,16 +5,18 @@
  *
  * @s: string
  *
- * Return: s
+ * Return: string
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 }
