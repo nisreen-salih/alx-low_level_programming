890d8b0833d517e2ed56b32d2a0f64a167c9225f
Nisreen <fg.nisreen@gmail.com>
2023-10-01 03:12:43 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,8 +13,10 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y <= 1)
+	else if (y == 1)
 		return (x);
+	else if (y == 0)
+		return (1);
 	else
 		return (_pow_recursion(x, y - 1) * x);
 }
