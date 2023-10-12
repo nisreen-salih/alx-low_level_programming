ac4a7e3f28430ca6f1bbd987137174bd4bbcc7d5
Nisreen <fg.nisreen@gmail.com>
2023-10-11 18:19:47 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+nclude "3-calc.h"
+
+/**
+ * main - check code
+ * @argc : args num
+ * @argv: arg vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), x, y;
+
+	if (argc != 4)
+	{
+		printf("Error\n"), exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n"), exit(99);
+	}
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		prinf("Error\n"), exit(100);
+	}
+	printf("%d\n", po_func(x, y));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add int
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract int
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - multiply int
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - divide int
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - module int
+ *
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
