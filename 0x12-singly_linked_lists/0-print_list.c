49cf367e9464c2e70587b0dfe705950a859be001
nisreen-salih <fg.nisreen@gmail.com>
2023-10-19 15:45:38 -0700

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * strlen - string length
+ *
+ * @c: string
+ *
+ * Return:  string length
+ */
+
+int _strlen(char *c)
+{
+	int s = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		s++;
+	return (s);
+}
+
+/**
+ * print_list - prints all the elements
+ *
+ * @h: first node pointer
+ *
+ * Return: list size
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+	}
+	return (c);
+}
