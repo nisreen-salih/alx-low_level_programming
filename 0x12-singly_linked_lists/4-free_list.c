e925ef1c60e4cb56e61e3faf7accfbc6bc3ececd
nisreen-salih <fg.nisreen@gmail.com>
2023-10-19 18:43:34 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: pointer
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *n,*nn;
+
+	if (!head)
+		return;
+	n = head;
+	while (n)
+	{
+		nn = n->next;
+		free(n->str);
+		free(n);
+		n = nn;
+	}
+}
