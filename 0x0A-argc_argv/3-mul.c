31b54be3cd1e48debb08028dea72f25ed6a12599
nisreen-salih <fg.nisreen@gmail.com>
2023-10-25 12:48:29 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: commmandlines
+ * @argv: pointer
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+				printf("%d\n", mul);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
+}
