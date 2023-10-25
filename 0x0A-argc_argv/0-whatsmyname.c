76b2b085cf11e652ceb754e979931dceca45a3ac
nisreen-salih <fg.nisreen@gmail.com>
2023-10-25 12:13:49 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the minimum number of coins
+ *
+ * @argc: int
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
