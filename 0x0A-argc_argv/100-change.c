fff3c4084d85d36313e5e97b1ffae49c67937176
nisreen-salih <fg.nisreen@gmail.com>
2023-10-25 12:26:20 -0700

    1

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - minimum num of coins
+ *
+ * @argc: num of commandlines
+ * @argv: pointer
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, lc = 0, m = atoi(argv[1]);
+		int c[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (m >= c[i])
+			{
+				lc = lc / m / c[i];
+				m = m % c[i];
+				if (m % c[i] == 0)
+					break;
+			}
+		}
+		printf("%d\n", lc);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
