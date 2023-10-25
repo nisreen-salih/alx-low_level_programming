b21e9a6355651b169db9ee3401b10e24692f01da
nisreen-salih <fg.nisreen@gmail.com>
2023-10-25 01:24:01 -0700

    103

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ *
+ * @head: first pointer
+ */
+
+
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop -  finds the loop in a linked list
+ *
+ * @head: list
+ *
+ * Return: address or NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *p, *e;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	for (e = head->next; e != NULL; e = e->next)
+	{
+		if (e == e->next)
+		{
+			return (e);
+		}
+		for (p = head; p != e; p = p->next)
+			if (p == e->next)
+				return (e->next);
+	}
+	return (NULL);
+}
