96287f3f956b2763f1a0cfe38fcdbee2d77ba7a0
nisreen-salih <fg.nisreen@gmail.com>
2023-11-23 08:10:38 -0800

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: num
+ *
+ * Return: null
+ */
+
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8, pr = 0;
+
+	while (b)
+	{
+		if (n & 1L << --b)
+		{
+			_putchar('1');
+			pr++;
+		}
+		else if (pr)
+		{
+			_putchar('0');
+		}
+		if (!pr)
+		{
+			_putchar('0');
+		}
+	}
+}
