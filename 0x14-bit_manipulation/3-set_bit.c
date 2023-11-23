0e60aa1fc0c8dba13b46b346c33a252a88538ab8
nisreen-salih <fg.nisreen@gmail.com>
2023-11-23 08:25:22 -0800

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - ets the value of a bit to 0
+ *
+ * @n: index num
+ * @index: bit
+ *
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	return (!!(*n |= 1L << index));
+}
